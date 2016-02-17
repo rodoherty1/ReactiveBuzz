af63779e7cccb049b1329eb3c5363127286d7c78
Rob O'Doherty <rodoherty1@gmail.com>
2016-02-17 22:22:53 +0000

    Now authenticating with Twitter.  Still a long way to go

--- src/main/scala/io/rob/Main.scala
+++ src/main/scala/io/rob/Main.scala
@@ -1,20 +1,13 @@
 package io.rob
 
 import java.nio.charset.StandardCharsets
-import java.security.cert.CertificateFactory
-
 import akka.actor.{ActorSystem, Props}
-import akka.http.scaladsl.model.{HttpMethods, HttpMethod, Uri, HttpRequest}
-import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.{DefaultSSLContextCreation, Http, HttpsContext}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.headers.{RawHeader, BasicHttpCredentials, Authorization}
 import akka.stream.ActorMaterializer
-import akka.util.ByteString
 import com.typesafe.scalalogging.LazyLogging
 
-import java.security.cert.{ CertificateFactory, Certificate }
-import java.security.{SecureRandom, KeyStore}
-import javax.net.ssl.{ SSLParameters, SSLContext, TrustManagerFactory, KeyManagerFactory }
-import java.io.InputStream
 import java.util.Base64
 
 import io.rob.CommonDefs.FetchReactiveBuzz
@@ -26,19 +19,33 @@ object Main extends App with LazyLogging {
   implicit val system = ActorSystem()
   implicit val materializer = ActorMaterializer()
 
+  implicit val executionContext = system.dispatcher
 
-  Http().singleRequest(HttpRequest(uri = Uri("https://api.twitter.com/oauth2/token"), method = HttpMethods.POST)).onComplete {
-    case Success(r) ⇒ {
+  val key = ""
+  val secret = ""
+
+  val encodedKeySecret = Base64.getEncoder.encodeToString(s"$key:$secret".getBytes(StandardCharsets.UTF_8))
+
+  val authorization = Authorization(BasicHttpCredentials(key, secret))
+  val contentType: HttpHeader = RawHeader("Content-Type", "application/x-www-form-urlencoded;")
+
+  Http().singleRequest(HttpRequest(
+    uri = Uri("https://api.twitter.com/oauth2/token"),
+    method = HttpMethods.POST,
+    headers = List(authorization, contentType),
+    entity = FormData("grant_type" -> "client_credentials").toEntity,
+    protocol = HttpProtocols.`HTTP/1.1`)).onComplete {
+
+    case Success(r) ⇒
       logger.info(r.toString)
       val reporter = system.actorOf(Props[Reporter], "Reporter")
       logger.info("Checking out the buzz about #Reactive applications")
-      reporter ! FetchReactiveBuzz
-    }
+       reporter ! FetchReactiveBuzz
     case Failure(e) ⇒ logger.error(e.getMessage, e)
   }
 
   protected def base64Encode(key: String, secret: String): String = {
-    Base64.getEncoder.encodeToString("user:pass".getBytes(StandardCharsets.UTF_8))
+    Base64.getEncoder.encodeToString(s"$key:$secret".getBytes(StandardCharsets.UTF_8))
   }
 
 }
